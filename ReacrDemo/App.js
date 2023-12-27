d6731c24544bec24efc7be7d5212f1fc836766fd
aksh-200 <81736871+aksh-200@users.noreply.github.com>
2023-12-28 00:12:37 +0530

    ADDED ROUTER , PASSING DATA USING NAVIGATE HOOK

---
+++ ReacrDemo/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Ajaxfun2 from './Ajaxex';
+import Details from './details';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="" element={<Ajaxfun2 />}/>
+        
+
+        <Route path="/details" element={<Details />} /> 
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
